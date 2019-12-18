993c59f2f24300f1d9924e83acff4a200a3362b6
Iñaki Silanes <isilanes@lortek.es>
2019-12-18 18:59:28 +0100

    SquareIntoSquares: minor changes, still wrong

--- 4kyu/SquareIntoSquares/square_into_squares.py
+++ 4kyu/SquareIntoSquares/square_into_squares.py
@@ -6,10 +6,16 @@ def decompose(n):
     result = [n-1]
     remainder = 2*n - 1  # n**2 - (n-1)**2
     while remainder > 0:
+        if remainder == 2:  # because then last two insertions are 1, 1
+            return None
+        
         # 'next' is largest N such that N**2 <= remainder:
         next = int(math.sqrt(remainder))
-        result.append(next)
+        if remainder == 2*next:
+            next -= 1
+
         remainder -= next**2
+        result.append(next)
 
     return result[::-1]
 
