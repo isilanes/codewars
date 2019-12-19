f39ec3b83bbff7f8c9009225f2243279dbe9c90b
Iñaki Silanes <isilanes@lortek.es>
2019-12-19 17:07:13 +0100

    SquareIntoSquares: solved

--- 4kyu/SquareIntoSquares/square_into_squares.py
+++ 4kyu/SquareIntoSquares/square_into_squares.py
@@ -2,38 +2,67 @@
 
 import math
 
+class Snake:
+
+    def __init__(self, n):
+        self.members = [n-1]
+        self.remaining = n**2 - (n-1)**2
+
+    def next_value(self):
+        return min(self.last-1, int(math.sqrt(self.remaining)))
+
+    def grow(self, value):
+        self.members.append(value)
+        self.remaining -= value**2
+
+    def chomp(self):
+        self.remaining += self.last**2
+        self.members = self.members[:-1]
+
+    def decrement_last(self):
+        last = self.last
+        self.members[-1] -= 1
+        self.remaining += last**2 - (last-1)**2
+
+    @property
+    def last(self):
+        return self.members[-1]
+
+    @property
+    def size(self):
+        return len(self.members)
 
 def decompose(n):
-    snake = [n-1]
-    rem = n**2 - (n-1)**2
+    snake = Snake(n)
 
     while True:
-        print("DEBUG11", snake, rem)
-        next = min(snake[-1]-1, int(math.sqrt(rem)))
-        rem -= next**2
+        if snake.remaining == 0:
+            return snake.members[::-1]
 
-        if rem == 0:
-            snake.append(next)
-            return snake[::-1]
+        next = snake.next_value()
 
-        if next > 1:
-            snake.append(next)
+        if next == 1 and snake.remaining != 1:
+            snake.decrement_last()
+            while snake.last == 1:
+                snake.chomp()
+                if snake.size == 0:
+                    return None
+
+                snake.decrement_last()
+
             continue
 
-        if snake[-1] == 1:
-            snake = snake[:-1]
-
-        snake[-1] -= 1
+        snake.grow(next)
 
     return None
 
 
 
 if __name__ == "__main__":
-    #assert decompose(5) == [3, 4]
-    #assert decompose(8) is None
-    #assert decompose(11) == [1, 2, 4, 10]
+    assert decompose(5) == [3, 4]
+    assert decompose(8) is None
+    assert decompose(11) == [1, 2, 4, 10]
     assert decompose(12) == [1, 2, 3, 7, 9]
-    assert decompose(50) == [1, 2, 5, 8, 49]
+    assert decompose(50) == [1, 3, 5, 8, 49]
     assert decompose(7100) == [2, 3, 5, 119, 7099]
-    assert decompose(7654321) == [6, 10, 69, 39,12, 7654320]
+    assert decompose(7654321) == [6, 10, 69, 3912, 7654320]
