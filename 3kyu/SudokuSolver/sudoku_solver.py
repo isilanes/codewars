888c4c1df366318da8e14eeca4c9641df16b364c
Iñaki Silanes <isilanes@gmail.com>
2019-12-25 23:18:43 +0100

    SudokuSolver: start building

--- 3kyu/SudokuSolver/sudoku_solver.py
+++ 3kyu/SudokuSolver/sudoku_solver.py
@@ -1,17 +1,53 @@
 # https://www.codewars.com/kata/5296bc77afba8baa690002d7
 
+
 class Board:
 
-    def __init__(self):
-        pass
+    def __init__(self, initial):
+        self.initial = self._to_1d(initial)
+
+    @staticmethod
+    def _to_1d(two_d_array):
+        res = []
+        for line in two_d_array:
+            res.extend(line)
+
+        return res
 
 
 def sudoku(puzzle):
-    return True
+    board = Board(puzzle)
+
+    print(board.initial)
+
+    return None
 
 
 def main():
-    pass
+    puzzle = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    answer = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9],
+    ]
+
+    assert sudoku(puzzle) == answer
 
 
 if __name__ == "__main__":
