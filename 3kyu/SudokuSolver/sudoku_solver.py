68cfa90942e1f6283c6f2ce2a875d89c61a49b74
Iñaki Silanes <isilanes@gmail.com>
2019-12-25 17:54:56 +0100

    start solving sudoku problem

---
+++ 3kyu/SudokuSolver/sudoku_solver.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/5296bc77afba8baa690002d7
+
+class Board:
+
+    def __init__(self):
+        pass
+
+
+def sudoku(puzzle):
+    return True
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
