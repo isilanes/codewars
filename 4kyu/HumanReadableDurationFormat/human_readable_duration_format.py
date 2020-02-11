387d9199e70021ebe2f87706c5d790235f653248
Iñaki Silanes <isilanes@lortek.es>
2020-02-11 17:44:56 +0100

    Solve HumanReadableDurationFormat

--- 4kyu/HumanReadableDurationFormat/human_readable_duration_format.py
+++ 4kyu/HumanReadableDurationFormat/human_readable_duration_format.py
@@ -1,12 +1,39 @@
 # https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
 
 
+PERIODS = [
+    (365*24*3600, "year"),
+    (24*3600, "day"),
+    (3600, "hour"),
+    (60, "minute"),
+    (1, "second"),
+]
+
+
 def format_duration(seconds):
-    return None
+    if not seconds:
+        return "now"
+
+    fragments = []
+    for dt, unit in PERIODS:
+        if seconds >= dt:
+            n = seconds // dt
+            if n > 1:
+                unit = f"{unit}s"
+            fragments.append(f"{n} {unit}")
+            seconds = seconds % dt
+
+    if len(fragments) == 1:
+        return fragments[0]
+
+    comma_separated = ", ".join(fragments[:-1])
+
+    return f"{comma_separated} and {fragments[-1]}"
 
 
 def main():
     in_and_out = [
+        (0, "now"),
         (1, "1 second"),
         (62, "1 minute and 2 seconds"),
         (120, "2 minutes"),
