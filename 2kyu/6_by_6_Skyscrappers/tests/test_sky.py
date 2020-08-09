65f51052a0c77a83160fb21d540930755616e5fd
Iñaki Silanes <isilanes@gmail.com>
2020-08-09 18:20:05 +0200

    Added two more tests that show that algo is very sub-optimal

--- 2kyu/6_by_6_Skyscrappers/tests/test_sky.py
+++ 2kyu/6_by_6_Skyscrappers/tests/test_sky.py
@@ -21,7 +21,7 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(seen_from_right((1, 2, 3, 6, 5, 4)), 3)
         self.assertEqual(seen_from_right((1, 3, 2, 6, 5, 4)), 3)
 
-    def test_solver(self):
+    def test_case_1(self):
         clues = (
             3, 2, 2, 3, 2, 1,
             1, 2, 3, 3, 2, 2,
@@ -37,4 +37,34 @@ class TestFunctions(unittest.TestCase):
             (5, 4, 1, 6, 2, 3),
             (3, 2, 5, 4, 6, 1)
         )
+        self.assertEqual(solve_puzzle(clues), expected)
+
+    def test_case_2(self):
+        clues = (0, 0, 0, 2, 2, 0,
+                 0, 0, 0, 6, 3, 0,
+                 0, 4, 0, 0, 0, 0,
+                 4, 4, 0, 3, 0, 0)
+
+        expected = ((5, 6, 1, 4, 3, 2),
+                    (4, 1, 3, 2, 6, 5),
+                    (2, 3, 6, 1, 5, 4),
+                    (6, 5, 4, 3, 2, 1),
+                    (1, 2, 5, 6, 4, 3),
+                    (3, 4, 2, 5, 1, 6))
+
+        self.assertEqual(solve_puzzle(clues), expected)
+
+    def test_case_3(self):
+        clues = (0, 3, 0, 5, 3, 4,
+                 0, 0, 0, 0, 0, 1,
+                 0, 3, 0, 3, 2, 3,
+                 3, 2, 0, 3, 1, 0)
+
+        expected = ((5, 2, 6, 1, 4, 3),
+                    (6, 4, 3, 2, 5, 1),
+                    (3, 1, 5, 4, 6, 2),
+                    (2, 6, 1, 5, 3, 4),
+                    (4, 3, 2, 6, 1, 5),
+                    (1, 5, 4, 3, 2, 6))
+
         self.assertEqual(solve_puzzle(clues), expected)
