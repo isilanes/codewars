a7e5480c16d5d98eecd371d4857a78fefde13897
Iñaki Silanes <isilanes@gmail.com>
2020-08-13 20:29:13 +0200

    Left solution_05.py as-is, borked

--- 1kyu/7_by_7_Skyscrappers/solution_05.py
+++ 1kyu/7_by_7_Skyscrappers/solution_05.py
@@ -47,7 +47,6 @@ class Puzzle:
         self.all_combos = list(permutations(range(1, N_ELEMENTS+1)))
 
         self.skipped_for_step = [0 for _ in range(2 * N_ELEMENTS)]
-        self.valids_for_position = [None for _ in range(2 * N_ELEMENTS)]
         self.combo_for_step = [None for _ in range(2 * N_ELEMENTS)]
         self.placement_to_index = [None for _ in range(2 * N_ELEMENTS)]
         self.is_row_or_col = [None for _ in range(2 * N_ELEMENTS)]
@@ -145,17 +144,24 @@ class Puzzle:
 
     def prepare_to_go_back(self, i_placement):
         self.skipped_for_step[i_placement] = 0
-        self.valids_for_position[i_placement] = None
         self.placement_to_index[i_placement] = None
         self.skipped_for_step[i_placement - 1] += 1
+        self.combo_for_step[i_placement] = None
+        self.combos_for_rows_at_step[i_placement] = [None for _ in range(N_ELEMENTS)]
+        self.combos_for_cols_at_step[i_placement] = [None for _ in range(N_ELEMENTS)]
 
     def solve(self):
-        print([len(c) for c in self.combos_for_row])
-        print([len(c) for c in self.combos_for_col])
+        print([len(self.combos_for_row[i]) for i in range(7)])
+        print([len(self.combos_for_col[i]) for i in range(7)])
 
         i_placement = 0
-        while i_placement < 3:
+        while i_placement < 5:
+            if i_placement < 0:
+                print("fallo")
+                break
+
             combo = self.place_nth_combo(i_placement)
+
             if combo is None:
                 self.prepare_to_go_back(i_placement)
                 i_placement -= 1
@@ -163,116 +169,23 @@ class Puzzle:
 
             i_placement += 1
 
-        print("\n----")
+        print("\n---")
         return self.solution
 
-    def calc_valids_for_nth_placement(self, i_placement) -> bool:
-        previous_rows = [self.placement_to_index[i] for i in range(i_placement) if self.is_row_or_col[i] == "row"]
-        previous_cols = [self.placement_to_index[i] for i in range(i_placement) if self.is_row_or_col[i] == "col"]
-
-        # Rows:
-        min_index, min_valids, min_n_valids, min_which = -1, [], None, None
-        for i, combos in enumerate(self.combos_for_row):
-            if i in previous_rows:
-                continue
-
-            valids = []
-            for combo in combos:
-                can_be = True
-                for prev_placement in range(i_placement):
-                    # Row vs col:
-                    if self.is_row_or_col[prev_placement] == "col":
-                        i_row_cross = i
-                        i_col_cross = self.placement_to_index[prev_placement]
-                        if self.combo_for_step[prev_placement][i_row_cross] != combo[i_col_cross]:
-                            can_be = False
-                            break
-
-                    # Row vs row:
-                    else:
-                        if not are_parallel_combos_congruent(combo, self.combo_for_step[prev_placement]):
-                            can_be = False
-                            break
-
-                if can_be:
-                    valids.append(combo)
-
-            n_valids = len(valids)
-            if min_n_valids is None or n_valids < min_n_valids:
-                if n_valids == 0:
-                    return False
-
-                min_index, min_valids, min_n_valids, min_which = i, valids, n_valids, "row"
-
-        # Cols:
-        for i, combos in enumerate(self.combos_for_col):
-            if i in previous_cols:
-                continue
-
-            valids = []
-            for combo in combos:
-                can_be = True
-                for prev_placement in range(i_placement):
-                    # Col vs row:
-                    if self.is_row_or_col[prev_placement] == "row":
-                        i_col_cross = i
-                        i_row_cross = self.placement_to_index[prev_placement]
-                        if self.combo_for_step[prev_placement][i_col_cross] != combo[i_row_cross]:
-                            can_be = False
-                            break
-
-                    # Col vs col:
-                    else:
-                        if not are_parallel_combos_congruent(combo, self.combo_for_step[prev_placement]):
-                            can_be = False
-                            break
-
-                if can_be:
-                    valids.append(combo)
-
-            n_valids = len(valids)
-            if min_n_valids is None or n_valids < min_n_valids:
-                if n_valids == 0:
-                    return False
-
-                min_index, min_valids, min_n_valids, min_which = i, valids, n_valids, "col"
-
-        self.valids_for_position[i_placement] = min_valids
-        self.placement_to_index[i_placement] = min_index
-        self.is_row_or_col[i_placement] = min_which
-
-        return True
-
-    def set_combo(self, i) -> Union[list, None]:
-        try:
-            proposed_combo = self.valids_for_position[i][self.skipped_for_step[i]]
-            self.combo_for_step[i] = proposed_combo
-
-            return proposed_combo
-        except IndexError:
-            return None
-
-    def choose_combo(self, i_placement) -> None:
-        if i_placement == 0:
-            buena = (1, 4, 5, 6, 7, 2, 3)
-            self.combo_for_step[i_placement] = buena
-            return
-
-        if i_placement == 1:
-            buena = (2, 3, 1, 4, 6, 5, 7)
-            self.combo_for_step[i_placement] = buena
-            return
+    def choose_combo(self, i_placement) -> bool:
+        idx = self.placement_to_index[i_placement]
+        skipped = self.skipped_for_step[i_placement]
 
         if self.is_row_or_col[i_placement] == "row":
             valids = self.combos_for_rows_at_step[i_placement]
         else:
             valids = self.combos_for_cols_at_step[i_placement]
 
-        if i_placement == 1:
-            buena = (2, 3, 1, 4, 6, 5, 7)
-            print("DEBUG270", buena in valids[self.placement_to_index[i_placement]])
-
-        self.combo_for_step[i_placement] = valids[self.placement_to_index[i_placement]][self.skipped_for_step[i_placement]]
+        try:
+            self.combo_for_step[i_placement] = valids[idx][skipped]
+            return True
+        except IndexError:
+            return False
 
     def prune_next_valids(self, i_placement):
         previous_row_indices, previous_col_indices = [], []
@@ -287,7 +200,7 @@ class Puzzle:
         if self.is_row_or_col[i_placement] == "row":
             for i_col, v in enumerate(combo):
                 if i_col in previous_col_indices:
-                    self.combos_for_cols_at_step[i_placement+1][i_col] = []
+                    #self.combos_for_cols_at_step[i_placement+1][i_col] = []
                     continue
 
                 new_valids = [c for c in self.combos_for_cols_at_step[i_placement][i_col] if c[i_current] == v]
@@ -298,7 +211,7 @@ class Puzzle:
 
             for i_row in range(N_ELEMENTS):
                 if i_row == i_current or i_row in previous_row_indices:
-                    self.combos_for_rows_at_step[i_placement+1][i_row] = []
+                    #self.combos_for_rows_at_step[i_placement+1][i_row] = []
                     continue
 
                 new_valids = [c for c in self.combos_for_rows_at_step[i_placement][i_row] if are_parallel_combos_congruent(combo, c)]
@@ -310,7 +223,7 @@ class Puzzle:
         else:  # col
             for i_col in range(N_ELEMENTS):
                 if i_col == i_current or i_col in previous_col_indices:
-                    self.combos_for_cols_at_step[i_placement+1][i_col] = []
+                    #self.combos_for_cols_at_step[i_placement+1][i_col] = []
                     continue
 
                 new_valids = [c for c in self.combos_for_cols_at_step[i_placement][i_col] if are_parallel_combos_congruent(combo, c)]
@@ -321,7 +234,7 @@ class Puzzle:
 
             for i_row, v in enumerate(combo):
                 if i_row in previous_row_indices:
-                    self.combos_for_rows_at_step[i_placement+1][i_row] = []
+                    #self.combos_for_rows_at_step[i_placement+1][i_row] = []
                     continue
 
                 new_valids = [c for c in self.combos_for_rows_at_step[i_placement][i_row] if c[i_current] == v]
@@ -345,6 +258,9 @@ class Puzzle:
             if i in previous_row_indices:
                 continue
 
+            if combos is None:
+                print(i_placement, self.combos_for_rows_at_step[i_placement])
+
             if min_index is None or len(combos) < min_n_valids:
                 min_index, min_n_valids, min_which = i, len(combos), "row"
 
@@ -358,23 +274,25 @@ class Puzzle:
         self.is_row_or_col[i_placement] = min_which
         self.placement_to_index[i_placement] = min_index
 
-        if i_placement == 0:
-            self.is_row_or_col[i_placement] = "col"
-            self.placement_to_index[i_placement] = 2
-
-        if i_placement == 1:
-            self.is_row_or_col[i_placement] = "row"
-            self.placement_to_index[i_placement] = 0
+        print("DEBUG285", min_which, min_index)
 
     def place_nth_combo(self, i_placement: int) -> Union[list, None]:
         if self.placement_to_index[i_placement] is None:
             self.calc_min_location(i_placement)
 
-        self.choose_combo(i_placement)
-        print("DEBUG341", self.is_row_or_col[i_placement], self.placement_to_index[i_placement], self.combo_for_step[i_placement])
-        success = self.prune_next_valids(i_placement)
+        success = self.choose_combo(i_placement)
         if not success:
+            print("esto no debe pasar nunca")
             return None
+
+        print("DEBUG339", i_placement, self.is_row_or_col[i_placement],
+              self.placement_to_index[i_placement], self.combo_for_step[i_placement])
+
+        if i_placement < (2 * N_ELEMENTS - 1):
+            success = self.prune_next_valids(i_placement)
+            if not success:
+                print("pruned to death")
+                return None
 
         return self.combo_for_step[i_placement]
 
